e7b13a17763ffe5edf90270e1dc458fe5c4244e9
Nick Been <66799549+nickbeen@users.noreply.github.com>
2022-02-23 19:46:02 +0100

    Add files (#1)

---
+++ openssl-updater.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# Simple shell script to update a custom OpenSSL installation
+
+# Set branch version
+openssl="openssl-1.1.1"
+
+if [ -z "$1" ]; then
+  echo  "[?] Usage: ./openssl-updater.sh https://www.openssl.org/source/${openssl}{version}.tar.gz"
+  exit 1
+else
+  if [[ "$1" != *.tar.gz ]]; then
+    echo "[X] ERROR: Resource is not a tar.gz file"
+    exit 1
+  fi
+fi
+
+# Get file name
+file="$(basename "$1")"
+
+# Download tar.gz file with Wget
+wget --no-clobber --output-document="${file}" --quiet "$1"
+
+if [ $? -eq 0 ]; then
+  echo "[V] Downloaded ${file}"
+else
+   echo "[X] ERROR: Cannot download ${file}"
+   exit 1
+fi
+
+# Unpack tar.gz file
+tar --extract --file "${file}" --gzip --one-top-level
+
+if [ $? -eq 0 ]; then
+  echo "[V] Unpacked ${file}"
+else
+  echo "[X] ERROR: Cannot unpack ${file}"
+  exit 1
+fi
+
+# Get directory name
+directory="$(basename "$1" .tar.gz)"
+
+# Create or update symlink
+ln --force --no-dereference --symbolic "${directory}" "${openssl}"
+
+if [ $? -eq 0 ]; then
+  echo "[V] Updated ${openssl} symlink"
+else
+  echo "[X] ERROR: Cannot update ${openssl} symlink"
+  exit 1
+fi
+
+# Remove downloaded tar.gz file
+unlink "${file}"
+
+if [ $? -eq 0 ]; then
+  echo "[V] Removed ${file}"
+else
+  echo "[X] ERROR: Cannot remove ${file}"
+  exit 1
+fi
